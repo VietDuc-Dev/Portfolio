595f205fa6f02e51fd6f32e8fb52129d5def8ac9
vietduc-dev <levietduc.dev@gmail.com>
2025-12-13 20:44:56 +0700

    feat(page): project detail page

--- app/(public)/projects/[slug]/page.tsx
+++ app/(public)/projects/[slug]/page.tsx
@@ -1,5 +1,133 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaYoutube, FaGlobe } from "react-icons/fa";
 
 export default function ProjectDetailPage() {
-  return <div>ProjectDetailPage</div>;
+  return (
+    <section className="max-w-6xl mx-auto px-6 py-4">
+      {/* Preview */}
+      <div className="relative rounded-2xl overflow-hidden shadow-2xl mb-12">
+        {/* <Image
+          src="/projects/portfolio-preview.png" // thay bằng ảnh của bạn
+          alt="Professional Portfolio Template"
+          width={1400}
+          height={800}
+          className="object-cover w-full"
+          priority
+        /> */}
+        <img
+          className="w-full object-cover transition-transform duration-[700ms] ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-[1.08] "
+          src="https://cdn.sanity.io/images/fpx3afrv/production/c9735ae7e20ef26c90fc7e1607c2bfdc08097792-4320x4320.png?w=1080&h=1080&fm=webp&q=85"
+          alt="Sunset in the mountains"
+        />
+      </div>
+
+      {/* Content */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
+        {/* LEFT */}
+        <div className="lg:col-span-2">
+          <h1 className="text-4xl font-bold tracking-tight mb-4">
+            Professional Portfolio Template
+          </h1>
+
+          <p className="text-sm mb-8">
+            Featured: <span className="font-medium">No</span>
+          </p>
+
+          {/* Tech stack */}
+          <div className="mb-8">
+            <h3 className="font-semibold mb-3">Tech Stack</h3>
+            <div className="flex flex-wrap gap-2">
+              {[
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Getform",
+                "Google Fonts",
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Getform",
+                "Google Fonts",
+              ].map((tech) => (
+                <span
+                  key={tech}
+                  className="px-3 py-1 text-xs font-medium rounded-full glass"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          {/* Features */}
+          <div className="mb-10">
+            <h3 className="font-semibold mb-3">Project Features</h3>
+            <ul className="space-y-2 text-sm list-disc pl-5">
+              <li>Modern and fully responsive UI</li>
+              <li>Fast performance with minimal HTML & CSS</li>
+              <li>Contact form integration with Getform</li>
+              <li>Mobile tab navigation setup</li>
+            </ul>
+          </div>
+
+          {/* Project Links */}
+          <div>
+            <h3 className="font-semibold mb-3">Project Links</h3>
+            <div className="flex flex-wrap gap-4">
+              <Button variant="outline" className="hover:text-white">
+                <FaGithub /> Github
+              </Button>
+              <Button variant="outline" className="hover:text-white">
+                <FaGlobe /> Live
+              </Button>
+              <Button variant="outline" className="hover:text-white">
+                <FaYoutube /> Youtube
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT */}
+        <aside className="space-y-6">
+          {/* CTA */}
+          {/* <div className="rounded-2xl border p-6 shadow-sm">
+            <button className="w-full bg-yellow-400 hover:bg-yellow-500 text-black font-semibold py-3 rounded-lg transition">
+              Purchase Premium Code
+            </button>
+          </div> */}
+          {/* Connect */}
+          <div className="rounded-2xl border p-6 shadow-sm">
+            <h3 className="font-semibold mb-4">Contact me</h3>
+            <div className="flex flex-wrap gap-4">
+              <Button variant="outline" className="hover:text-white">
+                <FaYoutube /> Zalo
+              </Button>
+              <Button variant="outline" className="hover:text-white">
+                <FaYoutube /> Email
+              </Button>
+              <Button variant="outline" className="hover:text-white">
+                <FaYoutube /> Phone
+              </Button>
+            </div>
+          </div>
+        </aside>
+      </div>
+    </section>
+  );
+}
+
+function SocialIcon({ icon }: { icon: React.ReactNode }) {
+  return (
+    <Link
+      href="#"
+      className="
+        p-3 rounded-full border
+        hover:bg-muted transition
+      "
+    >
+      {icon}
+    </Link>
+  );
 }
--- components/common/CardProject.tsx
+++ components/common/CardProject.tsx
@@ -1,30 +1,34 @@
 "use client";
 
+import { ArrowUpRight, Github } from "lucide-react";
+import { Button } from "../ui/button";
+
 export default function CardProject() {
   return (
-    <div className="group rounded overflow-hidden flex flex-col max-w-xl mx-auto transition-all duration-500 hover:-translate-y-2 hover:shadow-[0_20px_40px_rgba(0,0,0,0.15)]">
+    <div className="group rounded overflow-hidden flex flex-col max-w-xl mx-auto transition-all duration-500 hover:-translate-y-2">
       {/* IMAGE */}
-      <a href="#" className="overflow-hidden">
+      <a href="projects/123" className="overflow-hidden">
         <img
-          className="w-full object-cover transition-transform duration-[700ms] ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-[1.08] "
-          src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=625&sharp=10&w=1000"
+          className="w-full max-h-52 object-cover transition-transform duration-[700ms] ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:scale-[1.08] "
+          src="https://cdn.sanity.io/images/fpx3afrv/production/c9735ae7e20ef26c90fc7e1607c2bfdc08097792-4320x4320.png?w=1080&h=1080&fm=webp&q=85"
           alt="Sunset in the mountains"
         />
       </a>
 
       {/* CONTENT */}
-      <div className="relative -mt-16 px-4 py-5 glassBase m-10 transition-all duration-500 group-hover:-translate-y-1 group-hover:backdrop-blur-lg">
+      <div className="relative -mt-12 px-4 py-2 glassBase m-10 transition-all duration-500 group-hover:-translate-y-1 group-hover:backdrop-blur-lg text-black/80">
         <a
-          href="#"
-          className="font-semibold text-lg inline-block mb-2 transition-colors duration-300 group-hover:text-blue-500"
+          href="projects/123"
+          className="font-semibold text-lg inline-block transition-colors duration-300 group-hover:text-black"
         >
           Teams Project Management App
         </a>
-
-        <p className="text-sm opacity-90 group-hover:opacity-100 transition-opacity duration-300">
-          Today, I’m covering one of my favorite parts of the Nordstrom
-          Anniversary Sale: the activewear!
-        </p>
+        <Button variant="link" className="rounded-sm text-gray-800">
+          <Github /> Github
+        </Button>
+        <Button variant="link" className="rounded-sm text-gray-800">
+          <ArrowUpRight /> Demo
+        </Button>
       </div>
     </div>
   );
