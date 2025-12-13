4b5e77a24b5e0cba4226f7872b66157059b17420
vietduc-dev <levietduc.dev@gmail.com>
2025-12-13 14:38:22 +0700

    feat(page): UI blog page

--- app/(public)/blog/page.tsx
+++ app/(public)/blog/page.tsx
@@ -1,10 +1,41 @@
-import Container from "@/components/common/Container";
-import React from "react";
+"use client";
+
+import { DropdownMenuCheckboxes } from "@/components/features/FilterProject";
+import { motion } from "framer-motion";
 
 export default function BlogPage() {
   return (
-    <Container>
-      <h1>Blog</h1>
-    </Container>
+    <motion.div
+      initial={{ opacity: 0, y: 12 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {/* Header */}
+      <div className="flex justify-between items-center mb-6">
+        <motion.h1
+          initial={{ opacity: 0, x: -15 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="
+            text-3xl font-bold tracking-tight bg-linear-to-r 
+            from-pink-500 to-blue-500 bg-clip-text text-transparent
+          "
+        >
+          Blog
+        </motion.h1>
+
+        <motion.div
+          initial={{ opacity: 0, x: 12 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.3, duration: 0.5 }}
+          className="space-x-4 flex"
+        >
+          <DropdownMenuCheckboxes title="Sort" />
+        </motion.div>
+      </div>
+
+      {/* Grid (loading vs real data) */}
+      <div className="grid grid-cols-3 gap-4">...</div>
+    </motion.div>
   );
 }
