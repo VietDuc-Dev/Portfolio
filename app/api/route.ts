8660ea61c67c62fe8c96060952664909245d7e5f
vietduc-dev <levietduc.dev@gmail.com>
2025-12-16 01:43:59 +0700

    chore: update api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,26 +1,10 @@
 import { NextResponse } from "next/server";
-import React from "react";
 
-export async function GET(req: NextResponse) {
+export async function GET() {
   try {
     return NextResponse.json({ message: "Hello" }, { status: 201 });
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ message: "" }, { status: 500 });
-  }
-}
-
-export async function POST(req: NextResponse) {
-  const { firstName, lastName, email, password } = await req.json();
-
-  //   await dbConnect();
-
-  try {
-    // const user = await User.findOne({email});
-    // LOGIC ...
-    return NextResponse.json({ message: "" }, { status: 201 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ message: "" }, { status: 500 });
+    return NextResponse.json({ message: "Error system" }, { status: 500 });
   }
 }
